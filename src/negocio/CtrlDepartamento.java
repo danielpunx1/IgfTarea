5b80a2504ab4000f5cd4bec15cc238291c55af36
enri_pin <enri_pin@yahoo.com>
2014-11-04 11:46:12 -0600

    Clases de Negocio: CtrlMunicipio, CtrlDepartamento

---
+++ src/negocio/CtrlDepartamento.java
@@ -0,0 +1,52 @@
+package negocio;
+
+import java.util.List;
+
+import dao.DepartamentoDAO;
+import dominio.Departamento;
+
+public class CtrlDepartamento {
+	private DepartamentoDAO daoDepto = new DepartamentoDAO();
+
+	public boolean crearDepartamento(String id_depto, String nombre_depto, String zona_geografica) {
+		if ((daoDepto.daDepartamentoByIdDepto(id_depto) == null)&&(daoDepto.daDepartamentoByNombreDepto(nombre_depto)==null)) {
+			Departamento departamento = new Departamento(id_depto, nombre_depto, zona_geografica);
+			daoDepto.guardaActualiza(departamento);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean eliminarDepartamento(String id_depto) {
+		if (daoDepto.daDepartamentoByIdDepto(id_depto)!= null) {
+			Departamento departamento = daoDepto.daDepartamentoByIdDepto(id_depto);
+			daoDepto.eliminar(departamento);
+			return true;
+		} else
+			return false;
+	}
+
+	public boolean modificarDepartamento(String id_depto, String nombre_depto, String zona_geografica) {
+		if (daoDepto.daDepartamentoByIdDepto(id_depto) != null) {
+			Departamento departamento = daoDepto.daDepartamentoByIdDepto(id_depto);
+			departamento.setNombreDepto(nombre_depto);
+			departamento.setZonaGeografica(zona_geografica);
+			daoDepto.guardaActualiza(departamento);
+			return true;
+		} else
+			return false;
+	}
+	
+	public List<Departamento> daDepartamentos() {
+		return daoDepto.daDepartamentos();
+	}
+
+	public Departamento daDepartamentoByIdDepto(String id_depto) {
+		return daoDepto.daDepartamentoByIdDepto(id_depto);
+	}
+
+	public Departamento daDepartamentoByNombreDepto(String nombre_depto) {
+		return daoDepto.daDepartamentoByNombreDepto(nombre_depto);
+	}
+}
---
+++ src/negocio/CtrlMunicipio.java
@@ -0,0 +1,60 @@
+package negocio;
+
+import java.util.List;
+import dao.DepartamentoDAO;
+import dao.MunicipioDAO;
+import dominio.Departamento;
+import dominio.Municipio;
+
+public class CtrlMunicipio {
+	private MunicipioDAO daoMun = new MunicipioDAO();
+	private DepartamentoDAO daoDepto = new DepartamentoDAO();
+
+	public boolean crearMunicipio(String id_municipio, String id_depto, String nomb_municipio) {
+		if ((daoMun.daMunicipioByIdMunicipio(id_municipio)== null)&&(daoDepto.daDepartamentoByIdDepto(id_depto)!=null)) {
+			Departamento departamento = daoDepto.daDepartamentoByIdDepto(id_depto);
+			Municipio municipio = new Municipio(id_depto, departamento, nomb_municipio);
+			daoMun.guardaActualiza(municipio);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean eliminarMunicipio(String id_municipio) {
+		if (daoMun.daMunicipioByIdMunicipio(id_municipio)!= null) {
+			Municipio municipio = daoMun.daMunicipioByIdMunicipio(id_municipio);
+			daoMun.eliminar(municipio);
+			return true;
+		} else
+			return false;
+	}
+
+	public boolean modificarMunicipio(String id_municipio, String id_depto, String nomb_municipio) {
+		if (daoMun.daMunicipioByIdMunicipio(id_municipio) != null) {
+			Departamento departamento = daoDepto.daDepartamentoByIdDepto(id_depto);
+			Municipio municipio = daoMun.daMunicipioByIdMunicipio(id_municipio);
+			municipio.setIdDepto(departamento);
+			municipio.setNombMunicipio(nomb_municipio);
+			daoMun.guardaActualiza(municipio);
+			return true;
+		} else
+			return false;
+	}
+	
+	public List<Municipio> daMunicipio() {
+		return daoMun.daMunicipios();
+	}
+
+	public Municipio daMunicipioByIdMunicipio(String id_municipio) {
+		return daoMun.daMunicipioByIdMunicipio(id_municipio);
+	}
+
+	public Municipio daMunicipioByNombMunicipio(String nomb_municipio) {
+		return daoMun.daMunicipioByNombreNombMunicipio(nomb_municipio);
+	}
+	
+	public List<Municipio> daMunicipioByIdDepto(String id_depto){
+		return daoMun.daMunicipiosByIdDepto(id_depto);
+	}
+}
