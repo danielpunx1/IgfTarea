c198dcab4e0da1e4bb0babd49291556cd8bded15
Diego Monterroza <rudel.mvp1@gmail.com>
2014-11-05 00:48:59 -0600

    Modificados y finalizados a nivel de clases, pendiente con los jsp

--- src/negocio/CtrlOficina.java
+++ src/negocio/CtrlOficina.java
@@ -1,16 +1,22 @@
 package negocio;
 
+import java.util.List;
+
+import dao.MunicipioDAO;
 import dao.OficinaDAO;
 import dominio.Municipio;
 import dominio.Oficina;
 
 public class CtrlOficina {
 	private OficinaDAO daoOficina = new OficinaDAO();
-	
-	public boolean crearOficina(String id_oficina, Municipio municipio, String nomb_oficina,
+	private MunicipioDAO daoMunicipio = new MunicipioDAO();
+
+	public boolean crearOficina(String id_oficina, String id_municipio, String nomb_oficina,
 			String domicilio){
-		
-		if(daoOficina.daOficinaById(id_oficina)==null){
+
+		if(daoOficina.daOficinaByIdOficina(id_oficina)==null
+				&&daoMunicipio.daMunicipioByIdMunicipio(id_municipio)!=null){
+			Municipio municipio = daoMunicipio.daMunicipioByIdMunicipio(id_municipio);
 			Oficina oficina = new Oficina(id_oficina, municipio, nomb_oficina, domicilio);
 			daoOficina.guardaActualiza(oficina);
 			return true;
@@ -18,5 +24,50 @@ public class CtrlOficina {
 		else {
 			return false;
 		}
+	}
+
+	public boolean eliminarOficina(String id_oficina){
+		Oficina oficina = daoOficina.daOficinaByIdOficina(id_oficina);
+		if(oficina!=null){
+			daoOficina.eliminar(oficina);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean modificarOficina(String id_oficina, String id_municipio, String nomb_oficina,
+			String domicilio){
+		
+		if(daoOficina.daOficinaByIdOficina(id_oficina)!=null
+				&&daoMunicipio.daMunicipioByIdMunicipio(id_municipio)!=null){
+			Municipio municipio = daoMunicipio.daMunicipioByIdMunicipio(id_municipio);
+			Oficina oficina = daoOficina.daOficinaByIdOficina(id_oficina);
+			oficina.setMunicipio(municipio);
+			oficina.setDomicilio(domicilio);
+			oficina.setNomb_oficina(nomb_oficina);
+			daoOficina.guardaActualiza(oficina);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public Oficina daOficinaByIdOficina(String id_oficina){
+		return daoOficina.daOficinaByIdOficina(id_oficina);
+	}
+	
+	public List<Oficina> daOficinas(){
+		return daoOficina.daOficinas();
+	}
+	
+	public Oficina daOficinaByNombOficina(String nomb_oficina){
+		return daoOficina.daOFicinaByNombOficina(nomb_oficina);
+	}
+	
+	public List<Oficina> daOficinaByMunicipio(String id_municipio){
+		return daoOficina.daOficinasByIdMunicipio(id_municipio);
 	}
 }
